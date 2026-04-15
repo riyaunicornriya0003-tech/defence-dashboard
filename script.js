0f3ceae8d16fbb22fe9cb7319125e6bcf955b59f
Riya S B <riyaunicornriya0003@gmail.com>
2026-04-15 22:02:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function simulate() {
   alert("🚀 Simulation Running...\nAll units deployed!");
 }
+
+function login() {
+  let user = document.getElementById("user").value;
+  let pass = document.getElementById("pass").value;
+
+  if (user === "admin" && pass === "1234") {
+    window.location.href = "index.html";
+  } else {
+    alert("❌ Access Denied");
+  }
+}
