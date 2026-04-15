89de5600a0bcf66f2bda01f367d177821c760a02
Riya S B <riyaunicornriya0003@gmail.com>
2026-04-15 22:10:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function simulate() {
-  alert("🚀 Simulation Running...\nAll units deployed!");
-}
-
 function login() {
   let user = document.getElementById("user").value;
   let pass = document.getElementById("pass").value;
+
+  alert("User: " + user + " | Pass: " + pass);
 
   if (user === "admin" && pass === "1234") {
     window.location.href = "index.html";
